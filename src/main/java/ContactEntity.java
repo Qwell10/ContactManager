b4e430520813973e4a260849c7a5d0885907e07e
Qwell10 <mihal.chaloupka@seznam.cz>
2025-07-08 19:21:13 +0200

    Console app complete. Next step: Refactor to Spring with DB

---
+++ src/main/java/ContactEntity.java
@@ -0,0 +1,9 @@
+public record ContactEntity(String name, String phoneNumber, String email) {
+
+    @Override
+    public String toString() {
+        return String.format("Name: %s, Phone number: %s, Email: %s",
+                name(), phoneNumber(), email());
+    }
+}
+
