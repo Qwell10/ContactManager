00a8f550edd6a303813db687addcb383627905d5
Qwell10 <mihal.chaloupka@seznam.cz>
2026-01-01 19:59:44 +0100

    config for soap

--- src/main/java/com/contactManager/config/WebServiceConfig.java
+++ src/main/java/com/contactManager/config/WebServiceConfig.java
@@ -1,11 +1,26 @@
 package com.contactManager.config;
 
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
 
+
+// todo("@Profile("soap")" -> later to lear Spring profiles
+
+
+@EnableWs //This enables the SOAP message handling
 @Configuration
-public class WebServiceConfig {
+public class WebServiceConfig extends WsConfigurerAdapter {
 
     /**
      * Defines the Marshaller/Unmarshaller for JAXB2 - translate between java object and XML(language to store and transport data)
@@ -17,5 +32,28 @@ public class WebServiceConfig {
         marshaller.setContextPath("com.contactManager.soap.gen");
 
         return marshaller;
+    }
+
+    @Bean
+    public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext context) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(context);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean<>(servlet, "/ws/*");
+    }
+
+    @Bean(name = "contacts")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema usersSchema) {
+        DefaultWsdl11Definition wsdl = new DefaultWsdl11Definition();
+        wsdl.setPortTypeName("ContactsPort");
+        wsdl.setLocationUri("/ws");
+        wsdl.setTargetNamespace("http://whereareyou/contacts");
+        wsdl.setSchema(usersSchema);
+        return wsdl;
+    }
+
+    @Bean
+    public XsdSchema usersSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("contact.xsd"));
     }
 }
---
+++ src/main/java/com/contactManager/soap/ContactSoapEndpoint.java
@@ -0,0 +1,12 @@
+package com.contactManager.soap;
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+
+@Endpoint
+public class ContactSoapEndpoint {
+    //todo(endpoint like REST)
+
+    private static final String NAMESPACE_URI = "http://whereareyou/contacts";
+
+
+}
